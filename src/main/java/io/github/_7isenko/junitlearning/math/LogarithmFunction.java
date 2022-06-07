a172a2abfe289dcdf089b36113cbe3b04ea3ee02
7isenko <7isenko.max@gmail.com>
2022-06-08 02:12:49 +0300

    created templates for basic functions

---
+++ src/main/java/io/github/_7isenko/junitlearning/math/LogarithmFunction.java
@@ -0,0 +1,13 @@
+package io.github._7isenko.junitlearning.math;
+
+import java.util.function.DoubleFunction;
+
+/**
+ * @author 7isenko
+ */
+public class LogarithmFunction implements DoubleFunction<Double> {
+    @Override
+    public Double apply(double value) {
+        return null;
+    }
+}
