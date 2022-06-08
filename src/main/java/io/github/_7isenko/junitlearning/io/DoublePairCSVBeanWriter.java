5fa095696d1858fcabd2dea90c1b8cc508d303a4
7isenko <7isenko.max@gmail.com>
2022-06-09 01:01:45 +0300

    changed catching exception

--- src/main/java/io/github/_7isenko/junitlearning/io/DoublePairCSVBeanWriter.java
+++ src/main/java/io/github/_7isenko/junitlearning/io/DoublePairCSVBeanWriter.java
@@ -3,13 +3,12 @@ package io.github._7isenko.junitlearning.io;
 import com.opencsv.CSVWriter;
 import com.opencsv.bean.StatefulBeanToCsv;
 import com.opencsv.bean.StatefulBeanToCsvBuilder;
-import com.opencsv.exceptions.CsvFieldAssignmentException;
+import com.opencsv.exceptions.CsvException;
 
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Writer;
-import java.nio.file.Path;
 
 /**
  * @author 7isenko
@@ -33,7 +32,7 @@ public class DoublePairCSVBeanWriter {
     public void write(DoublePairCSVBean pair) throws IOException {
         try {
             sbc.write(pair);
-        } catch (CsvFieldAssignmentException e) {
+        } catch (CsvException e) {
             throw new IOException("Exception during csv field assignment: " + e.getMessage());
         }
     }
